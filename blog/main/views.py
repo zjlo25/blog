68eeba3a37e2e9bffcd984283a87d040fc92a0fe
zjlo25 <john88012500306@gmail.com>
2018-10-29 14:39:39 +0800

    First push

---
+++ blog/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def main(request):
+    '''
+    Show 'Hello world!' in the main page
+    '''
+    return HttpResponse('Hello world!')
+
+# Create your views here.
